547cc2d068f0c6e0959dc12406d97e49ea2ce583
MauLight <maulisseluz@gmail.com>
2023-06-17 16:42:55 -0400

    front end added, log in screen

---
+++ front-end/src/App.jsx
@@ -0,0 +1,19 @@
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import './App.css'
+import { Login } from './views/Login'
+import { Home } from './views/Home'
+
+function App() {
+
+  return (
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_OAUTH_ID}>
+      <Routes>
+        <Route path='login' element={<Login />} />
+        <Route path='/*' element={<Home />} />
+      </Routes>
+    </GoogleOAuthProvider>
+  )
+}
+
+export default App
---
+++ front-end/src/views/Login.jsx
@@ -0,0 +1,33 @@
+import { GoogleLogin, googleLogout } from '@react-oauth/google'
+import { useNavigate } from "react-router-dom"
+import { FcGoogle } from 'react-icons/fc'
+import heroVideo from '../assets/Hero_video.mp4'
+import logo from '../assets/1.png'
+
+export const Login = () => {
+    return (
+        <div className="flex justify-start items-center flex-col h-screen">
+            <div className="relative w-full h-full">
+                <video
+                    src={heroVideo}
+                    type='video/mp4'
+                    loop
+                    controls={false}
+                    muted
+                    autoPlay
+                    className="object-cover w-full h-full"
+                />
+                <div className="absolute flex flex-col justify-center items-center top-0 left-0 right-0 bottom-0 bg-blackOverlay">
+                    <div className="p-5">
+                        <img src={logo} width='230px' alt="logo" />
+                    </div>
+                    <div className="shadow-2xl">
+                        <GoogleLogin 
+                        onSuccess={(response) => console.log(response)}
+                        onError={() => console.log("Error!")} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
