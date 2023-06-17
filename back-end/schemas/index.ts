5ff5576151ee8c1fbaf9f0fc3a45812074745eaa
MauLight <maulisseluz@gmail.com>
2023-06-17 14:28:11 -0400

    first commit

---
+++ back-end/schemas/index.ts
@@ -0,0 +1,13 @@
+import user from './user'
+import pin from './pin'
+import save from './save'
+import postedBy from './postedBy'
+import comments from './comments'
+
+export const schemaTypes = [
+    user,
+    pin,
+    save,
+    postedBy,
+    comments
+]
