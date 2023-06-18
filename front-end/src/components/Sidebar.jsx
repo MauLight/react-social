aa981ca72c1e30ab6cec5920330131e1eb26e172
MauLight <maulisseluz@gmail.com>
2023-06-17 23:59:33 -0400

    sidebar added, home nav working

--- front-end/src/components/Sidebar.jsx
+++ front-end/src/components/Sidebar.jsx
@@ -1,7 +1,23 @@
+import { NavLink, Link } from 'react-router-dom'
+import { RiHomeFill } from 'react-icons/ri';
+import { IoIosArrowForward } from 'react-icons/io';
+import logo from '../assets/1.png'
 
-const Sidebar = () => {
+const Sidebar = ({ user, closeToggle }) => {
+
+
+
   return (
-    <div>Sidebar</div>
+    <div className="flex flex-col justify-between bg-white h-full overflow-y-scrikk min-w-210 hide-scrollbar">
+      <div className="flex flex-col">
+        <Link
+          to='/'
+          className='flex px-5 gap-2 my-6 pt-1 w-190 items-center'
+        >
+          <img src={logo} alt='logo' className='w-full' />
+        </Link>
+      </div>
+    </div>
   )
 }
 
--- front-end/src/views/Home.jsx
+++ front-end/src/views/Home.jsx
@@ -13,7 +13,12 @@ export const Home = () => {
 
   const [user, setUser] = useState(null)
   const [toggleSideBar, setToggleSideBar] = useState(false)
+  const scrollRef = useRef(null)
   const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+
+  useEffect(() => {
+    scrollRef.current.scrollTo(0, 0)
+  }, [])
 
   useEffect(() => {
     const query = userQuery(userInfo?.sub)
@@ -26,19 +31,20 @@ export const Home = () => {
   }, [])
 
   return (
-    <div className="flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out">
+    <div className="flex bg-gray-50 sd:flex-row flex-col h-screen transition-height duration-75 ease-out">
       <div className="hidden sd:flex h-screen flex-initial">
         <Sidebar user={user && user} closeToggle={setToggleSideBar} />
       </div>
       <div className="flex sd:hidden flex-row">
-        <Hamburger className='cursor-pointer' toggled={toggleSideBar} toggle={setToggleSideBar} />
-
-        <Link to='/'>
-          <img src={logo} alt="logo" className="w-28" />
-        </Link>
-        <Link to={`user-profile/${user?._id}`}>
-          <img src={user?.image} alt="logo" className="w-28" />
-        </Link>
+        <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
+          <Hamburger className='cursor-pointer' toggled={toggleSideBar} toggle={setToggleSideBar} />
+          <Link to='/'>
+            <img src={logo} alt="logo" className="w-28" />
+          </Link>
+          <Link to={`user-profile/${user?._id}`}>
+            <img src={user?.image} alt="logo" className="w-28" />
+          </Link>
+        </div>
       </div>
       {
         toggleSideBar && (
@@ -50,6 +56,12 @@ export const Home = () => {
           </div>
         )
       }
+      <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
+        <Routes>
+          <Route path="/user-profile/:userId" element={<UserProfile />} />
+          <Route path="/*" element={<Wall user={user && user} />} />
+        </Routes>
+      </div>
     </div>
   )
 }
